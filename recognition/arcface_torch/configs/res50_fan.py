8043766fc5c17b826504b5056e4af567148a2840
Hoàng Sơn <95064141+Akiyue@users.noreply.github.com>
2025-06-27 08:09:16 +0700

    Update res50_fan.py

--- recognition/arcface_torch/configs/res50_fan.py
+++ recognition/arcface_torch/configs/res50_fan.py
@@ -24,4 +24,5 @@ config.num_classes = 10575
 config.num_image = 494414
 config.num_epoch = 20
 config.warmup_epoch = 0
-config.val_targets = ['lfw', 'cfp_fp', "agedb_30"]
+config.val_targets = ['lfw']
+# config.val_targets = ['lfw', 'cfp_fp', "agedb_30"]
