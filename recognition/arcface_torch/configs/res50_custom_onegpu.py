b0cf3049edb49abb1e60d56207aa4a44d788be82
Hoàng Sơn <95064141+Akiyue@users.noreply.github.com>
2025-07-18 10:44:21 +0700

    Update res50_custom_onegpu.py

--- recognition/arcface_torch/configs/res50_custom_onegpu.py
+++ recognition/arcface_torch/configs/res50_custom_onegpu.py
@@ -1,1 +1,24 @@
+from easydict import EasyDict as edict
 
+config = edict()
+config.margin_list = (1.0, 0.5, 0.0)
+config.network = "r50"
+config.resume = False
+config.output = None
+config.embedding_size = 512
+config.sample_rate = 1.0
+config.fp16 = True
+config.momentum = 0.9
+config.weight_decay = 5e-4
+config.batch_size = 64
+config.lr = 0.02
+config.verbose = 2000
+config.dali = False
+
+config.rec = "/content/drive/MyDrive/colab/faces_webface_112x112"
+config.num_classes = 21083
+config.num_image = 501196
+config.num_epoch = 20
+config.warmup_epoch = 0
+# config.val_targets = ['lfw']
+# config.val_targets = ['lfw', 'cfp_fp', "agedb_30"]
